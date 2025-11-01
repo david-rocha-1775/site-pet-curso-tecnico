3c42596683030ca1c722425b8ede6670d6a167b4
david rocha <davrocha1006@gmail.com>
2025-11-01 15:37:47 -0300

    Update pet.php

--- pet.php
+++ pet.php
@@ -8,7 +8,7 @@ class pet{
     public $porte;
     public $foto;
 
-    public function _constructct($conn,$usuario_id, $nome, $sexo, $raca, $porte, $foto = null){
+    public function __construct($conn,$usuario_id, $nome, $sexo, $raca, $porte, $foto = null){
         $this ->conn = $conn ;
         $this->usuario_id = $usuario_id;
         $this ->nome = trim($nome);
@@ -20,9 +20,10 @@ class pet{
     }
     public function salvar(){
         $sql = "INSERT INTO pets (usuario_id ,nome, sexo, raca, porte, foto) VALUES (?,?,?,?,?,? ) ";
-        $stmt = $this->conn >prepare($sql);
 
-        if ($stmt ->execute([$this->usuario_id,$this->nome, $this->sexo, $this->raca, $this->porte, $this->foto])) {
+        $stmt = $this->conn->prepare($sql);
+
+        if ($stmt->execute([$this->usuario_id,$this->nome, $this->sexo, $this->raca, $this->porte, $this->foto])) {
             return $this->usuario_id;
         } else{
             throw new Exception("Erro ao salvar o pet.", 1);
