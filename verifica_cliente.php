91993fdf32978944b66c9b3c077de0a155e63ee4
david rocha <davrocha1006@gmail.com>
2025-11-14 23:01:50 -0300

    Update verifica_cliente.php

--- verifica_cliente.php
+++ verifica_cliente.php
@@ -1,12 +1,19 @@
 <?php
-include 'conexão.php' ;
-
 session_start();
-    if (!isset ($_SESSION['id'])) {
-        $_SESSION ['mensagem'] = "Faça o Login";
-        header('Location: pagina.php');
 
-        exit();
-    }
-    echo"Você está logado";
-    ?>
+// SE a sessão 'id' NÃO existir, o usuário não logou.
+if (!isset ($_SESSION['id'])) {
+    
+    // Opcional: Salva a mensagem de erro
+    $_SESSION ['mensagem'] = "Faça o Login";
+    
+    // Envia o usuário de volta para o LOGIN.
+    header('Location: index.html');
+    exit();
+}
+
+// SE o 'if' acima não rodou, significa que o usuário ESTÁ logado.
+// Então, redireciona para a página principal.
+header('Location: pagina.php');
+exit();
+?>
